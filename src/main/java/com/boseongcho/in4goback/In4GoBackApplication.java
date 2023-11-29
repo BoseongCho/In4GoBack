00fa0a20e25117bc4492b61ae40639aefb9be97e
TONY2\creba <creba1234@gmail.com>
2023-11-29 20:51:01 +0900

    first commit

---
+++ src/main/java/com/boseongcho/in4goback/In4GoBackApplication.java
@@ -0,0 +1,13 @@
+package com.boseongcho.in4goback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class In4GoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(In4GoBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boseongcho/in4goback/In4GoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boseongcho.in4goback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class In4GoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
