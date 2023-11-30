ee260843dda69ab3d3e787d20fd609c836704499
TONY2\creba <creba1234@gmail.com>
2023-11-30 20:53:04 +0900

    @autowired 버그 ->SuppressWarnings로 해결

--- src/main/java/com/boseongcho/in4goback/config/SecurityConfig.java
+++ src/main/java/com/boseongcho/in4goback/config/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig {
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     @Autowired
     public SecurityConfig(TokenProvider tokenProvider,
                           JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
