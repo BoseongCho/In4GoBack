cd6d53632fde82d9015ef5ed28c83bc1884e3c18
TONY2\creba <creba1234@gmail.com>
2023-11-30 13:38:25 +0900

    #1 기본세팅 완료

---
+++ src/main/java/com/boseongcho/in4goback/config/BeanConfiguration.java
@@ -0,0 +1,32 @@
+package com.boseongcho.in4goback.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfiguration {
+
+    /* 엔티티와 DTO 변환을 위한 modelMapper 빈 설정 */
+    @Bean
+    public ModelMapper modelMapper(){
+
+        return new ModelMapper();
+    }
+}
+
+
+/*
+  *ModelMapper (api)
+: DTO <-> Entity 객체로 변경해줌
+
+ -사용
+```java
+*하나 바꿔줄 때
+
+modelMapper.map(menuEntity, MenuDTO.class);
+                entity객체,   DTO.class
+
+*여러 개 (List)
+menuList.stream().map(menu -> modelMapper.map(menu, MenuDTO.class)).collect(Collectors.toList());
+* */
