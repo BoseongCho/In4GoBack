b8e1c88d16dcfd3deb4e434f966bdf7705ae499f
TONY2\creba <creba1234@gmail.com>
2023-12-01 14:28:23 +0900

    member-authority 구현 완료

--- src/main/java/com/boseongcho/in4goback/member/controller/AuthController.java
+++ src/main/java/com/boseongcho/in4goback/member/controller/AuthController.java
@@ -1,6 +1,14 @@
 package com.boseongcho.in4goback.member.controller;
 
+import com.boseongcho.in4goback.common.ResponseDTO;
+import com.boseongcho.in4goback.member.dto.MemberDTO;
 import com.boseongcho.in4goback.member.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +17,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
+
+    public AuthController(AuthService authService) {this.authService = authService;}
+    
+    /* @RequestBody를 통해 넘어온 Json 문자열을 파싱해서 MemberDTO 속성으로 매핑해 객체로 받아낸다.(회원 아이디, 비밀번호) */
+
+    @Operation(summary = "로그인 요청", description = "로그인 및 인증이 진행됩니다.", tags = {"AuthController"})
+    @PostMapping("/login")
+    public ResponseEntity<ResponseDTO> loging(@RequestBody MemberDTO memberDTO){
+        
+        return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "로그인 성공", authService.login(memberDTO)));
+        /*
+        ResponseEntity의 body메소드를 통해 Response객체의 body에 담기는 ResponseDTO는 JSON문자열이 되고
+        화면단의 React인 곳으로 가면 Store에 해당 리듀서가 관리하는 state값이 된다.
+        * */
+    }
 }
