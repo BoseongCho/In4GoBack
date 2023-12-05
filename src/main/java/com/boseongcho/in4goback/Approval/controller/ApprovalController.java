6cc23d34561b8e8bc85c2cfcc11c7d980fa69fe6
TONY2\creba <creba1234@gmail.com>
2023-12-05 13:07:02 +0900

    #10-approval-디렉토리-및-클래스-생성 완료

---
+++ src/main/java/com/boseongcho/in4goback/Approval/controller/ApprovalController.java
@@ -0,0 +1,14 @@
+package com.boseongcho.in4goback.Approval.controller;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+@AllArgsConstructor
+public class ApprovalController {
+
+
+}
---
+++ src/main/java/com/boseongcho/in4goback/Approval/dto/ApprovalMemDTO.java
@@ -0,0 +1,19 @@
+package com.boseongcho.in4goback.Approval.dto;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class ApprovalMemDTO {
+
+    private String memCode; //사원번호 PK
+    private String memName;
+
+    // FK
+//    private DepartmentDTO departmentCode; // 부서코드
+//    private PositionDTO positionCode; // 직급코드
+}
