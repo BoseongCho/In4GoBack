5bb1f096505b893b0c2e7733e9128420b94e29f6
TONY2\creba <creba1234@gmail.com>
2023-12-08 12:08:24 +0900

    approval modal 검색기능추가

--- src/main/java/com/boseongcho/in4goback/member/repository/MemberRepository.java
+++ src/main/java/com/boseongcho/in4goback/member/repository/MemberRepository.java
@@ -1,9 +1,22 @@
 package com.boseongcho.in4goback.member.repository;
 
+import com.boseongcho.in4goback.approval.entity.ApprovalMem;
 import com.boseongcho.in4goback.member.entity.Member;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, String> {
 
     Member findByMemCode(String memCode);
+
+//    @EntityGraph(value = "ApprovalMem.fetchAll", type = EntityGraph.EntityGraphType.LOAD)
+    @Query("SELECT DISTINCT m FROM ApprovalMem m WHERE m.memName LIKE concat('%', concat(:inputValue, '%'))")
+    List<ApprovalMem> getSearchInfoByName(String inputValue);
+
+//    @EntityGraph(value = "ArppvoalMem.fetchAll", type = EntityGraph.EntityGraphType.LOAD)
+//    @Query("SELECT DISTINCT m FROM ApprovalMem m WHERE m.positionCode.positionName LIKE concat('%', concat(:inputValue, '%'))")
+//    List<ApprovalMem> getSearchInfoByPosition(String inputValue);
 }
