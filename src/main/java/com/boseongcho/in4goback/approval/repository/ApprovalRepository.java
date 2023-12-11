be2561c966d8f326130012a6281c075eb3186fcc
TONY2\creba <creba1234@gmail.com>
2023-12-11 19:28:05 +0900

    #25-approval기간-별-서류-검색-구현-파일-업로드-구현

--- src/main/java/com/boseongcho/in4goback/approval/repository/ApprovalRepository.java
+++ src/main/java/com/boseongcho/in4goback/approval/repository/ApprovalRepository.java
@@ -14,6 +14,10 @@ public interface ApprovalRepository extends JpaRepository<Approval, String> {
     @Query("SELECT a FROM Approval a WHERE a.approvalMem.memCode = ?1")
     List<Approval> getApprovalList(String memCode);
 //    @EntityGraph(value = "Approval.fetchAll", type = EntityGraph.EntityGraphType.LOAD)
-    @Query("SELECT DISTINCT a FROM Approval a where a.approvalMem.memCode = ?1")
+    @Query("SELECT a FROM Approval a where a.approvalMem.memCode = ?1")
     Page<Approval> getApprovalList(String memCode, Pageable paging);
+
+    @Query("SELECT a FROM Approval a WHERE a.approvalMem.memCode = :memCode" +
+            " AND to_char(a.reportDate, 'YYYY-MM-DD') BETWEEN :startDate AND :endDate ORDER BY a.reportDate desc ")
+    List<Approval> getSearchApproval(String memCode, String startDate, String endDate);
 }
